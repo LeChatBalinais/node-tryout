2c22daeabe1fc2d4b29bc6d1b2f60209012d838e
Chat Balinais <chat.balinais@gmail.com>
2020-01-03 10:59:40 +0300

    Add Simple Example

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log('hello world!');
+import produce from 'immer';
+
+const baseState = [
+  {
+    todo: 'Learn typescript',
+    done: true
+  },
+  {
+    todo: 'Try immer',
+    done: false
+  }
+];
+
+const nextState = produce(baseState, draftState => {
+  const s = draftState;
+  s.push({ todo: 'Tweet about it', done: false });
+  s[1].done = true;
+});
+
+console.log(nextState);
