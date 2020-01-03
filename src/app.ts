79a5caa658ca13b3e695f5b5b4e0a6584303e3ec
Chat Balinais <chat.balinais@gmail.com>
2020-01-03 11:22:50 +0300

    replace,add, curry

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,7 @@
-console.log('hello world!');
+import { __, replace, add } from 'ramda';
+
+const greet = replace('{name}', __, 'Hello, {name}!');
+
+console.log(greet('Alice'));
+
+console.log(add(7, 10));
