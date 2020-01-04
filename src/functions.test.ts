a90e7ded85bf3cffba26d4d576c49f727f43ed68
Unknown <chat.balinais@gmail.com>
2020-01-04 13:51:39 +0300

    Add Jest

---
+++ src/functions.test.ts
@@ -0,0 +1,17 @@
+import functions from './functions';
+
+test('Adds 2 + 2 to equal 4', () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
+
+test('Adds 2 + 2 to equal 4', () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+test('Should be null', () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+test('Should be falsy', () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+});
---
+++ src/functions.ts
@@ -0,0 +1,6 @@
+const functions = {
+  add: (num1: number, num2: number): number => num1 + num2,
+  isNull: (): null => null,
+  checkValue: (x?: number): number => x
+};
+export default functions;
